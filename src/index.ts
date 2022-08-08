d086b9aa190dafa1629db6116662b062d3e17cdf
Alan Johnson <nalanj@fastmail.com>
2022-08-08 14:53:00 -0400

    Actually export memtable

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { TableBuilder } from "./table-builder";
 export { Table } from "./table";
 export { MergedTable } from "./merged-table";
 export type { Cursor } from "./cursor";
+export type { Memtable } from "./memtable";
